a0544e41adaf8abddbeff479ca4e5bd4dd440473
DimaDev <dilmurodrozikovvv@gmail.com>
2023-10-29 07:27:43 +0500

    INFRA: initialize unit test project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+﻿/*
+Copyright (c) is allowed for only 
+education reasons. Author : DimaDev.
+*/
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShoulBeTrue() => Assert.True(true);
+
+
+    }
+}
