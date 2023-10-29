374442a53340289cb3cd51eb9a4fe22537413753
DimaDev <dilmurodrozikovvv@gmail.com>
2023-10-29 12:06:27 +0500

    INFRA: Initialize Build Project

---
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Copyright (c) is allowed for only 
+education reasons. Author : DimaDev.
+*/
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Sheenam Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Setting up .NET",
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "6.0.300"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yml");
