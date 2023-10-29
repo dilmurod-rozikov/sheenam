9037d2c30b79423a84dfd4e2a52ee2048b998295
DimaDev <dilmurodrozikovvv@gmail.com>
2023-10-29 23:06:06 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.Api/Broker/Storages/Storagebroker.cs
@@ -0,0 +1,27 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Sheenam.Api.Broker.Storages
+{
+    public class Storagebroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public Storagebroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString); 
+        }
+        public override void Dispose(){ }
+        
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/GenderType.cs
@@ -0,0 +1,13 @@
+﻿/*
+Copyright (c) is allowed for only 
+education reasons. Author : DimaDev.
+*/
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public enum GenderType
+    {
+        Male,
+        Female
+    }
+}
